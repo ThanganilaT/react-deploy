3d2b3070eb684cf486109d0518489da7a13d516d
Thanganila <thnagnila2006@gmail.com>
2025-01-03 11:30:03 +0530

    change made

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="welcome-text">
-        <h1>Welcome to Our Website!</h1>
+        <h1>Welcome to my Website!!</h1>
         <p>Your one-stop solution for everything you need.</p>
       </div>
       <Footer />
